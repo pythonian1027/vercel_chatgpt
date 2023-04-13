ef0fb9bd613b66a599b3a671fc97f951c090460b
ricardocortez21@gmail.com <ricardocortez21@gmail.com>
2023-04-12 20:27:10 -0400

    updated main

---
+++ main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 12 19:06:49 2023
+
+@author: paco
+"""
+
+import os
+import openai
+
+openai.api_key = os.environ["OPENAI_API_KEY"]
+
+def ask_question(prompt):
+    # Your code here
+
+
+# from chatgpt import ask_question
+
+
+# question = "What is the capital of France?"
+# answer = ask_question(question)
+# print(answer)
